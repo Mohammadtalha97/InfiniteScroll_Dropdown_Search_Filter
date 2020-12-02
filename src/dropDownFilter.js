731878bf99ffc179dfc107e243c645c9f779c529
Mohammadtalha <mohammadtalha.patel@thegatewaycorp.com>
2020-12-02 13:00:50 +0530

    small changes

--- src/dropDownFilter.js
+++ src/dropDownFilter.js
@@ -18,25 +18,46 @@ const DropDownFilter = () => {
     let benderData = bender
       ? originalState.filter((x) => x.bender === bender)
       : originalState;
-
     let nationData = nation
       ? benderData.filter((x) => x.nation === nation)
       : benderData;
     let personData = person
       ? nationData.filter((x) => x.person === person)
       : nationData;
-
     let showData = show
       ? personData.filter((x) => x.show === show)
       : personData;
-
     const textSearchData = textSearch
       ? showData.filter((x) => x.name.includes(textSearch))
       : showData;
-
     setState(textSearchData);
-
-    console.log(textSearch);
+    //--------------------------------
+    // let benderData = bender
+    //   ? originalState.filter((x) => x.bender === bender)
+    //   : originalState;
+    // console.log("---->", benderData);
+    // setState(benderData);
+    // console.log("State", state);
+    // let nationData = nation
+    //   ? state.filter((x) => x.nation === nation)
+    //   : benderData;
+    // setState(nationData);
+    // let personData = person ? state.filter((x) => x.person === person) : state;
+    // setState(personData);
+    // let showData = show ? state.filter((x) => x.show === show) : state;
+    // setState(showData);
+    // const textSearchData = textSearch
+    //   ? state.filter((x) => x.name.includes(textSearch))
+    //   : state;
+    // setState(textSearchData);
+    //---------------------------
+    // let final = originalState.filter((x) => {
+    //   return bender
+    //     ? x.bender === bender
+    //     : x || nation
+    //     ? x.nation === nation
+    //     : x;
+    // });
   }, [bender, nation, person, show, textSearch]);
 
   let benderData = originalState.map((x) => x.bender);
@@ -54,46 +75,74 @@ const DropDownFilter = () => {
   return (
     <div>
       <label>Bender</label>
-      <select onChange={(e) => setBender(e.target.value)}>
+      <select
+        name="barber"
+        onChange={(e) => {
+          setBender(e.target.value);
+          localStorage.setItem([e.target.name], JSON.stringify(e.target.value));
+        }}
+      >
         <option value="">--Select--</option>
 
-        {uniqueBenderData.map((x) => (
-          <option value={x}>{x}</option>
+        {uniqueBenderData.map((x, index) => (
+          <option key={index} value={x}>
+            {x}
+          </option>
         ))}
       </select>
       <label>Nation</label>
-      <select onChange={(e) => setNation(e.target.value)}>
+      <select
+        name="nation"
+        onChange={(e) => {
+          setNation(e.target.value);
+          localStorage.setItem([e.target.name], JSON.stringify(e.target.value));
+        }}
+      >
         <option value="">--Select--</option>
-        {uniqueNationData.map((x) => (
-          <option value={x}>{x}</option>
+        {uniqueNationData.map((x, index) => (
+          <option key={index} value={x}>
+            {x}
+          </option>
         ))}
       </select>
       <label>Person</label>
       <select onChange={(e) => setPerson(e.target.value)}>
         <option value="">--Select--</option>
-        {uniquePersonData.map((x) => (
-          <option value={x}>{x}</option>
+        {uniquePersonData.map((x, index) => (
+          <option key={index} value={x}>
+            {x}
+          </option>
         ))}
       </select>
       <label>Show</label>
       <select onChange={(e) => setShow(e.target.value)}>
         <option value="">--Select--</option>
-        {uniqueShowData.map((x) => (
-          <option value={x}>{x}</option>
+        {uniqueShowData.map((x, index) => (
+          <option key={index} value={x}>
+            {x}
+          </option>
         ))}
       </select>
 
-      {/* <input type="text" onChange={(e) => setTextSearch(e.target.value)} /> */}
-      {/* <SelectSearch options={name} multiple search placeholder=" Search Name" /> */}
-      {state.length > 0 ? (
-        state.map((x) => (
-          <tr className="data">
-            <td key={x.name}>{x.name}</td>
-          </tr>
-        ))
-      ) : (
-        <h1>NoData</h1>
-      )}
+      <input type="text" onChange={(e) => setTextSearch(e.target.value)} />
+
+      <table>
+        <tbody>
+          {state.length > 0 ? (
+            state.map((x, index) => (
+              <tr key={index}>
+                <td>{x.name}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>
+                <h1>NoData</h1>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
